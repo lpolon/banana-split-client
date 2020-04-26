b3a4cc7184be1967b9c59fd4985ddbc05f7b3e85
lpolon <lpolon@gmail.com>
2020-04-25 23:35:06 -0300

    autheticated app receives user as props and renders it

--- src/AuthenticatedApp.jsx
+++ src/AuthenticatedApp.jsx
@@ -1,14 +1,12 @@
 import React, { Fragment } from 'react'
 
-export default function AuthenticatedApp({
-  user: { username = 'NO USER AND IT SHOULD NOT HAPPEN' },
-  logout,
-}) {
+export default function AuthenticatedApp({ user: { username, id }, logout }) {
   return (
     <Fragment>
       <h1>Logged In!</h1>
       <h2>username: {username}</h2>
-      <button onClick={logout}></button>
+      <h2>id: {id}</h2>
+      <button onClick={logout}>logout</button>
     </Fragment>
   )
 }
